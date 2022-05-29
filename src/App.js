d31e72af23a84e0e67ab5e0cd6ed6f3c59339222
Vega_AleksaMi <a.mitic@vegait.rs>
2022-05-29 18:25:11 +0200

    adding hooks examples

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from "react";
+import StateComponent from "./hooks/state";
+import "./styles.css";
 import { render } from "react-dom";
+import EffectComponent from "./hooks/effect";
+import ContextComponent from "./hooks/context";
+import RefComponent from "./hooks/ref";
+import MemoComponent from "./hooks/memo";
+import CallbackComponent from "./hooks/callback";
+import ImperativeHandleComponent from "./hooks/imperativeHandle";
+import LayoutEffectComponent from "./hooks/layoutEffect";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="App">
+      <StateComponent />
+      <hr />
+      <EffectComponent />
+      <hr />
+      <ContextComponent />
+      <hr />
+      <RefComponent />
+      <hr />
+      <MemoComponent />
+      <hr />
+      <CallbackComponent />
+      <hr />
+      <LayoutEffectComponent />
+      <hr />
+      <ImperativeHandleComponent />
     </div>
   );
 };
